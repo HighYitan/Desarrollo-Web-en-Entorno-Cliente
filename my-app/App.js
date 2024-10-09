27e7b0221531c605d14b8270702dd87e8612efb6
Khris <sr.highyitan@gmail.com>
2024-10-09 18:43:15 +0200

    'react'

--- my-app/App.js
+++ my-app/App.js
@@ -6,3 +6,27 @@ export function Profile(){
         />
     );
 }
+<section className="profile">
+    <h2>{name}</h2>
+    <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
+    />
+    <ul>
+    <li><b>Profession:</b> {profession}</li>
+    <li>
+        <b>Awards: {awards.length} </b>
+        ({awards.join(', ')})
+    </li>
+    <li>
+        <b>Discovered: </b>
+        {discovery}
+    </li>
+    </ul>
+</section>
+
+src={getImageUrl(person, (size<90) ? 's' : 'b')}
+{name} {(importance > 0) && <i>(Importance: {importance})</i>}
